7e545c88c6fa9bbf46b9633e9f998b56d8311c8c
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-01-28 09:51:28 +0200

    для роботи на уроці

---
+++ numbers.js
@@ -0,0 +1,58 @@
+// Парс числа з Number.parseInt() 
+// і Number.parseFloat()
+
+const elementWidth = '50px';
+// elementWidth = ;
+// console.log('elementWidth:', elementWidth);
+// ............
+
+let elementHeight = '200.74px';
+// elementHeight = ;
+// console.log('elementHeight:', elementHeight);
+
+// ...........
+// Метод число .toFixed(digits)
+// Матрьошки на прикладі console.log(Number(число, toFixed(digits)));
+//
+const salary = 1300.16472;
+
+// ...........
+
+// Переводимо (переобразовуємо) в число з Number(value)
+// Значення NaN (Not a Number) і метод Number.isNaN(value)
+// 
+let quantity = '30';
+let value = 'Цю строку неможливо перевести в число';
+
+// .............
+// Обєкт Math
+// - Піднесення до степені
+// - Exponent operator
+
+const base = 2;
+const power = 5;
+
+console.log();
+
+
+// ..............
+
+// Генеруємо псевдовипадкове число
+// - Math.random()
+// - Math.round()
+
+// ..............
+
+
+//Приклад використання
+// const colors = ['tomato', 'orange', 'blue', 'deeppink'];
+// const max = colors.length - 1;
+// const min = 0;
+
+// const index = Math.round((Math.random() * (max - min) + min));
+
+// const color = colors[index];
+
+// console.log(color);
+
+// document.body.style.backgroundColor = color;
